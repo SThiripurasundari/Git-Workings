9658c8d90d441a4d531f231fb7ce58e91509120d
SThiripurasundari <thiripurasundari.selvaraj@gmail.com>
2022-05-31 12:26:24 +0530

    V02 Will be added now

--- GitTest/src/GitRunSamples.java
+++ GitTest/src/GitRunSamples.java
@@ -1,6 +1,7 @@
 
 public class GitRunSamples {
 
+	private String two;
 	private String one;
 	
 	public static void main(String[] args) {
@@ -9,3 +10,17 @@ public class GitRunSamples {
 	}
 
 }
+
+/*
+ * …or create a new repository on the command line
+echo "# Git-Workings" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/SThiripurasundari/Git-Workings.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/SThiripurasundari/Git-Workings.git
+git branch -M main
+git push -u origin main*/
